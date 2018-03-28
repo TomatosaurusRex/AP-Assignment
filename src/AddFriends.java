7f6306f0fb09685b73e71b862c76f80714cf86f2
TomatosaurusRex <thomas.sc59@gmail.com>
2018-03-28 22:45:52 +1100

    Finished Code2

---
+++ src/AddFriends.java
@@ -0,0 +1,57 @@
+import java.sql.Statement;
+import java.util.Scanner;
+
+public class AddFriends {
+
+	public AddFriends() throws Exception{
+
+		System.out.println();
+		System.out.println("===================================");
+		System.out.println("	Who are your Friends?");
+		System.out.println("===================================");
+		System.out.println();
+
+		System.out.print("Enter an Username: ");
+		Scanner in1 = new Scanner(System.in);
+		String u1= in1.nextLine();
+
+		System.out.print("Enter an Friends UserName: ");	
+		Scanner in2 = new Scanner(System.in);
+		String u2= in2.nextLine();
+		
+		try {
+			com.mysql.jdbc.Connection con = Connectdb.getConnection("sql12228915","sql12228915","suS2gIQEFz");
+			Statement addUser = con.createStatement();  
+			addUser.executeUpdate("Insert Into UserFriend (UserName,FriendName) Values ('"+u1+"','"+u2+"')");    
+
+			con.close();
+			BackMenu();
+		}
+		catch(Exception e){ System.out.println("That username does not exist or they are already friends");
+		}
+	}
+	@SuppressWarnings({ "unused", "resource" })
+	public void BackMenu() throws Exception{
+
+		try{
+			System.out.println("Need to add another User?");
+			System.out.println("1.	Yes");
+			System.out.println("2.	Back to Main Menu");
+
+			Scanner keyboard = new Scanner(System.in);
+			System.out.print("Enter an option:");
+			int choice = keyboard.nextInt();
+			System.out.println();
+
+			if(choice==1){
+				AddUser nn = new AddUser();
+			}
+			else if (choice==2)
+			{
+				Menu su = new Menu();
+			}
+		}catch(Exception e){ System.out.println("Incorrect Imput. Please Try again");
+		BackMenu();}
+	}	
+	}
+
--- src/User.java
+++
@@ -1,9 +0,0 @@
-
-public class User {
-
-	public User(String username, String name, int age, String status) {
-		
-		
-	}
-
-}
